12dba1b6a4144edb62259610b3f1db743d966e0d
thiagop-usp <37089330+thiagop-usp@users.noreply.github.com>
2018-07-24 21:52:41 -0300

    Update kruskal.cpp

--- Graph Theory/kruskal.cpp
+++ Graph Theory/kruskal.cpp
@@ -10,33 +10,33 @@ using namespace std;
 
 const int MAXN = 1e5+5;
 
-tuple<int, int, int> edges[MAXN]; 			// this tuple is formed by 3 ints: <weight of the edge between u and v; vertice u; vertice v>
-int dad[MAXN]; 								// parent of each node
+tuple<int, int, int> edges[MAXN]; 	// this tuple is formed by 3 ints: <weight of the edge between u and v; vertice u; vertice v>
+int dad[MAXN]; 				// parent of each node
 
 int Find(int x){
-	if(x==dad[x]) return x; 				// if that node is the root of the tree (it's the pivot of the subset it's in)
-	return x = Find(dad[x]);				// keep climbing the tree to find its root
+	if(x==dad[x]) return x; 		// if that node is the root of the tree (it's the pivot of the subset it's in)
+	return x = Find(dad[x]);		// keep climbing the tree to find its root
 }
 
 void Union(int x, int y){
-	x = Find(x); 							// find pivot of the subset x's in
-	y = Find(y);							// find pivot of the subset y's in
-	if(x!=y) dad[x] = y; 					// if the pivots found are different, then make the Union
+	x = Find(x); 				// find pivot of the subset x's in
+	y = Find(y);				// find pivot of the subset y's in
+	if(x!=y) dad[x] = y; 			// if the pivots found are different, then make the Union
 }
 
 ll kruskal(int m){
 	ll cost = 0;
 	for (int i = 0; i < m; ++i)
 	{
-		int w = get<0>(edges[i]); 			// weight of the edge between u and v
-		int u = get<1>(edges[i]); 			// vertice u
-		int v = get<2>(edges[i]); 			// vertice v
-		if(Find(u)!=Find(v)){     			// if they have different pivots, i.e., if connecting u and v won't form a cycle
-			Union(u, v);          			// make the union
-			cost+=w;  						// add the cost to the total
+		int w = get<0>(edges[i]); 	// weight of the edge between u and v
+		int u = get<1>(edges[i]); 	// vertice u
+		int v = get<2>(edges[i]); 	// vertice v
+		if(Find(u)!=Find(v)){     	// if they have different pivots, i.e., if connecting u and v won't form a cycle
+			Union(u, v);          	// make the union
+			cost+=w;  		// add the cost to the total
 		}
 	}
-	return cost;							// returns the total weight of the Minimum Spanning Tree
+	return cost;				// returns the total weight of the Minimum Spanning Tree
 }
 
 int main(){
@@ -56,7 +56,7 @@ int main(){
 		edges[i] = make_tuple(w, u, v);
 	}
 
-	sort(edges, edges+m);					// sorting weights increasingly, since kruskal is a greedy algorithm and will add the m lightest edges
+	sort(edges, edges+m);	// sorting weights increasingly, since kruskal is a greedy algorithm and will add the m lightest edges
 
 	cout << kruskal(m) << endl;
 
